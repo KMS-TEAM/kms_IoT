b6b458d41c48edc6572710dca68c2ee2d33fa972
lacie-life <00sao00ios00@gmail.com>
2022-01-19 00:25:21 +0700

    [Update]

--- kms-Dashboard/hdr/AppModel.h
+++ kms-Dashboard/hdr/AppModel.h
@@ -2,15 +2,36 @@
 #define APPMODEL_H
 
 #include <QObject>
+#include <QString>
+#include <QMutex>
+#include "AppEnums.h"
+
+#define MODEL AppModel::getInstance();
 
 class AppModel : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(int currentScreenID READ currentScreenID WRITE setCurrentScreenID NOTIFY currentScreenIDChanged)
 public:
-    explicit AppModel(QObject *parent = nullptr);
+    static AppModel *getInstannce();
+    Q_INVOKABLE void qmlEventHandler(int event);
+
+    int currentScreenID() const;
+
+public slots:
+    void setCurrentScreenID(int currentScreenID);
 
 signals:
+    void currentScreenIDChanged(int currentScreenID);
 
+private:
+    AppModel(QObject* parent = nullptr);
+    AppModel(const AppModel&) = delete;
+    void operator =(const AppModel&) = delete;
+
+    static AppModel* m_instance;
+    static QMutex m_lock;
+    int m_currentScreenID;
 };
 
 #endif // APPMODEL_H
